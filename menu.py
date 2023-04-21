2ae84a9fd8a778edd0e96553eff572f24442eb2c
Briant Muller <bmuller18@gmail.com>
2023-04-20 22:36:08 -0400

    Configure Start button on escene

--- menu.py
+++ menu.py
@@ -1,23 +1,36 @@
-import pygame, sys
+import pygame as game, sys
 from pygame.locals import *
 import Resources.colors as colors
 import images as img
+import settings
 
 #Siempre ejecutar esta funcion
-pygame.init()
+game.init()
 
 #Screen config
-screen = pygame.display.set_mode((1000, 600))#Resolution
-pygame.display.set_caption('First game')#Titulo de la ventana
+screen = game.display.set_mode((settings.w, settings.h))#Resolution from settings (width, height)
+game.display.set_caption('First game')#Game title
 screen.fill(colors.rojo)
 
 #import images from python file
-pygame.display.set_icon(img.icon)
+game.display.set_icon(img.icon)
 screen.blit(img.bg, (0,0))#position of image
+
+#Start Button
+start_game = Rect(100, 100, 150, 50)#Button configure (Xpos, Ypos, width,)
+startgame = game.font.SysFont("Arial", 40)
+startbtn = startgame.render("Start", True, (50, 100, 150))
 #mantiene la ventana ejecutandose constantemente
 while True:
-    for event in pygame.event.get():
+    for event in game.event.get():
         if event.type == QUIT:
-            pygame.quit()
+            game.quit()
             sys.exit()
-    pygame.display.update()
+        game.draw.rect(screen, (70, 189, 34), start_game, 0)
+        
+        #Draw text into button rect
+        #Center X position text function-->(width buton + (button.width - text.get_with())/2, "height")
+        #Center y position text function-->(height buton + ("width", button.height - text.height())/2)
+        screen.blit(startbtn, (100+(start_game.width-startbtn.get_width())/2,100+(start_game.height - startbtn.get_height())/2))
+        
+    game.display.update()
---
+++ settings.py
@@ -0,0 +1,4 @@
+import pygame
+
+w = 1000
+h = 600
